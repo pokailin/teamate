3f97fd77526305ee7cde1adc4f53c36ae32fff11
pokailin <gerpros2020it1@gmail.com>
2023-05-08 22:13:53 +0800

    styling for nav

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="flex flex-col h-screen overflow-hidden">
-    <nav class="flex">
-      <div id="nav-logo-section">
+    <nav class="flex items-center">
+      <div id="nav-logo-section" class="nav-section">
         <NuxtLink to="/">
-          <Icon name="fa6-solid:dumpster-fire" />
+          <Icon name="ph:leaf-fill" class="text-green-500 text-3xl" />
+          <span class="ml-2">TEA MATE</span>
         </NuxtLink>
       </div>
 
-      <div id="nav-link-section">
+      <div id="nav-link-section" class="nav-section">
         <NuxtLink to="/about">ABOUT</NuxtLink>
 
         <NuxtLink to="/shop">SHOP</NuxtLink>
       </div>
 
-      <div id="nav-social-section">
+      <div id="nav-social-section" class="nav-section">
         <NuxtLink to="#">
           <Icon name="ph:twitter-logo-fill" />
         </NuxtLink>
@@ -27,7 +28,7 @@
         </NuxtLink>
       </div>
 
-      <div id="nav-contact-section">
+      <div id="nav-contact-section" class="nav-section">
         <NuxtLink to="/contact">CONTACT US</NuxtLink>
       </div>
     </nav>
@@ -40,10 +41,42 @@
 
 <style scoped lang="scss">
 div {
-  --border-color: rgb(255, 255, 255, 0.1);
+  // --background-color: rgb(6, 11, 25);
+  --background-color: rgb(226, 237, 227);
+  --border-color: rgba(25, 124, 47, 0.1);
+
+  background-color: var(--background-color);
 
   nav {
     border-bottom: 1px solid var(--border-color);
+
+    .nav-section {
+      @apply flex gap-8 items-center justify-center;
+      padding: 3rem 2rem;
+      border-left: 1px solid var(--border-color);
+
+      a {
+        color: black;
+      }
+    }
+
+    #nav-logo-section {
+      justify-content: flex-start;
+    }
+
+    #nav-link-section {
+      @apply gap-24;
+    }
+
+    #nav-logo-section,
+    #nav-link-section {
+      flex-basis: calc(100% / 3);
+    }
+
+    #nav-social-section,
+    #nav-contact-section {
+      flex-grow: 1;
+    }
   }
 }
 </style>
