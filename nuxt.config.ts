02761363a7c60abe5721906a10ddcd322a34ad24
pokailin <gerpros2020it1@gmail.com>
2023-05-08 21:51:36 +0800

    add nav bar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/css/main.scss"],
+  css: ['~/assets/css/main.scss'],
+  modules: ['nuxt-icon'],
   postcss: {
     plugins: {
       tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Home</h1>
+  <div class="flex flex-col h-screen overflow-hidden">
+    <nav class="flex">
+      <div id="nav-logo-section">
+        <NuxtLink to="/">
+          <Icon name="fa6-solid:dumpster-fire" />
+        </NuxtLink>
+      </div>
+
+      <div id="nav-link-section">
+        <NuxtLink to="/about">ABOUT</NuxtLink>
+
+        <NuxtLink to="/shop">SHOP</NuxtLink>
+      </div>
+
+      <div id="nav-social-section">
+        <NuxtLink to="#">
+          <Icon name="ph:twitter-logo-fill" />
+        </NuxtLink>
+
+        <NuxtLink to="#">
+          <Icon name="ph:youtube-logo-fill" />
+        </NuxtLink>
+
+        <NuxtLink to="#">
+          <Icon name="ph:instagram-logo-fill" />
+        </NuxtLink>
+      </div>
+
+      <div id="nav-contact-section">
+        <NuxtLink to="/contact">CONTACT US</NuxtLink>
+      </div>
+    </nav>
+
+    <main>
+      <h1>Home</h1>
+    </main>
+  </div>
 </template>
+
+<style scoped lang="scss">
+div {
+  --border-color: rgb(255, 255, 255, 0.1);
+
+  nav {
+    border-bottom: 1px solid var(--border-color);
+  }
+}
+</style>
