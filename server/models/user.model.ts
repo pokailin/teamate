cf8c155372b7b1349c011e3f97c036007b46888f
pokailin <gerpros2020it1@gmail.com>
2023-05-22 11:12:15 +0800

    add types for bcryptjs and jsonwebtoken / middleware for user password

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,15 +1,23 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: [true, 'Please enter your name'],
     },
     email: {
       type: String,
+      required: [true, 'Please enter your email'],
+      unique: true,
+      lowercase: true,
     },
     password: {
       type: String,
+      required: [true, 'Please enter a password'],
+      minlength: 8,
+      select: false,
     },
   },
   {
@@ -19,6 +27,16 @@ const userSchema = new mongoose.Schema(
     },
   }
 );
+
+userSchema.pre('save', async function (next) {
+  // Only has password if password is new or modified
+  if (!this.isModified('password')) return next();
+
+  // Hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
