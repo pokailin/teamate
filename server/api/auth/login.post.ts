5f3b72ff23cc7c0af5b9063b8a9de4793f3072b7
pokailin <gerpros2020it1@gmail.com>
2023-05-22 11:18:23 +0800

    setup login.post.ts

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,1 +1,30 @@
-export default defineEventHandler(async (event) => {});
+import User from '~/server/models/user.model';
+
+export default defineEventHandler(async (event) => {
+  const { email, password } = await readBody(event);
+
+  if (!email || !password) {
+    return createError({
+      statusCode: 400,
+      statusMessage: 'Please enter your email and password!',
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return createError({
+        statusCode: 401,
+        statusMessage: 'Incorrect email or password',
+      });
+    }
+
+    const token = generateAccessToken(user._id);
+
+    return {
+      status: 'success',
+      token,
+    };
+  } catch (e) {}
+});
--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -38,6 +38,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.methods.correctPassword = async function (
+  candidatePassword: string,
+  userPassword: string
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
