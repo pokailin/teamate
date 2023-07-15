b39c311b745b7718e4d6d06dffc4202a369e904b
pokailin <pokailin17@gmail.com>
2023-07-15 10:18:17 +0800

    setup tailwind

---
+++ assets/data/articles.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iure quaerat rem odit aliquam autem! Earum mollitia et beatae illo debitis quam quis ducimus praesentium?",
+    imgUrl:
+      "https://images.unsplash.com/photo-1624262536362-12cbb4965721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    subtitle: "Come Visit Our Store",
+  },
+  {
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iure quaerat rem odit aliquam autem! Earum mollitia et beatae illo debitis quam quis ducimus praesentium?",
+    imgUrl:
+      "https://images.unsplash.com/photo-1491497895121-1334fc14d8c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    subtitle: "Sourced from clean mountains",
+  },
+  {
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iure quaerat rem odit aliquam autem! Earum mollitia et beatae illo debitis quam quis ducimus praesentium?",
+    imgUrl:
+      "https://images.unsplash.com/photo-1582793988951-9aed5509eb97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+    subtitle: "Buy Tea",
+  },
+  {
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iure quaerat rem odit aliquam autem! Earum mollitia et beatae illo debitis quam quis ducimus praesentium?",
+    imgUrl:
+      "https://images.unsplash.com/photo-1531970227416-f0cddeb1f748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    subtitle: "Tea Appliances",
+  },
+];
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: ["@/assets/css/main.scss"],
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+  },
+});
